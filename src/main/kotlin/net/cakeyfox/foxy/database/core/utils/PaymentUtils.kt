0d22edc282833aa12297885a3a080e52e9b31fc4
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2025-10-17 21:20:54 -0300

    Fix `getCheckoutByUserId` returning approved checkouts

--- src/main/kotlin/net/cakeyfox/foxy/database/core/utils/PaymentUtils.kt
+++ src/main/kotlin/net/cakeyfox/foxy/database/core/utils/PaymentUtils.kt
@@ -1,13 +1,17 @@
 package net.cakeyfox.foxy.database.core.utils
 
+import com.mongodb.client.model.Filters.and
 import com.mongodb.client.model.Filters.eq
 import com.mongodb.client.model.UpdateOptions
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.firstOrNull
 import net.cakeyfox.foxy.database.core.DatabaseClient
 import net.cakeyfox.foxy.database.data.checkout.Checkout
+import net.cakeyfox.foxy.database.data.guild.Key
 import net.cakeyfox.foxy.database.data.store.StoreItem
 import net.cakeyfox.foxy.database.utils.builders.CheckoutBuilder
 import org.bson.Document
+import java.util.UUID
 
 class PaymentUtils(
     private val client: DatabaseClient
@@ -24,7 +28,12 @@ class PaymentUtils(
     suspend fun getCheckout(checkoutId: String): Checkout? {
         return client.withRetry {
             val checkouts = client.database.getCollection<Checkout>("checkoutlists")
-            val existingDocument = checkouts.find(eq("checkoutId", checkoutId)).firstOrNull()
+            val existingDocument = checkouts.find(
+                and(
+                    eq("checkoutId", checkoutId),
+                    eq("isApproved", false)
+                )
+            ).firstOrNull()
 
             existingDocument
         }
@@ -43,10 +52,35 @@ class PaymentUtils(
     suspend fun getCheckoutByUserId(userId: String): Checkout? {
         return client.withRetry {
             val checkouts = client.database.getCollection<Checkout>("checkoutlists")
-            val existingDocument = checkouts.find(eq("userId", userId))
+            val existingDocument = checkouts.find(
+                and(
+                    eq("userId", userId),
+                    eq("isApproved", false)
+                )
+            )
                 .firstOrNull()
 
             existingDocument
+        }
+    }
+
+    suspend fun registerKey(userId: String): Key {
+        val key = Key(
+            key = UUID.randomUUID()
+                .toString()
+                .replace("-", "")
+                .substring(0, 16)
+                .uppercase(),
+            ownedBy = userId,
+            usedBy = null
+        )
+
+        return client.withRetry {
+            val keys = client.database.getCollection<Key>("keys")
+            keys.find(eq("ownedBy", userId)).firstOrNull() ?: run {
+                keys.insertOne(key)
+                key
+            }
         }
     }
 
