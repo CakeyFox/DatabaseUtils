b21b2887f7c61669a1029f46d7ef2945efe54374
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2026-09-14 19:06:34 -0300

    Change `isAnnual` in `Checkout` to nullable to handle cases without an explicit annual flag

--- core/src/commonMain/kotlin/net/cakeyfox/foxy/database/common/data/checkout/Checkout.kt
+++ core/src/commonMain/kotlin/net/cakeyfox/foxy/database/common/data/checkout/Checkout.kt
@@ -6,5 +6,5 @@ data class Checkout(
     val itemId: String,
     val isApproved: Boolean,
     val paymentId: String? = null,
-    val isAnnual: Boolean = false
+    val isAnnual: Boolean? = false
 )
